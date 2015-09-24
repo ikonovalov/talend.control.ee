f614e705b15efd705714f55ed9fc9737d09913a2
ikonovalov <konovalov84@gmail.com>
2015-09-24 12:12:11 +0300

    Attach primefaces, jsf2.2

--- tcee-ejb/src/main/java/ru/codeunited/NewSessionBusiness.java
+++ tcee-ejb/src/main/java/ru/codeunited/NewSessionBusiness.java
@@ -1,14 +1,12 @@
 package ru.codeunited;
 
 import javax.ejb.Local;
-import javax.ejb.Remote;
 
 /**
  * codeunited.ru
  * konovalov84@gmail.com
  * Created by ikonovalov on 22.09.15.
  */
-@Remote
 @Local
 public interface NewSessionBusiness {
     void businessMethod();
---
+++ tcee-web/src/main/java/ru/codeunited/tcee/web/beans/TalendLogBean.java
@@ -0,0 +1,16 @@
+package ru.codeunited.tcee.web.beans;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * codeunited.ru
+ * konovalov84@gmail.com
+ * Created by ikonovalov on 24.09.15.
+ */
+@ManagedBean (name = "talendLog")
+@RequestScoped
+public class TalendLogBean {
+
+}
